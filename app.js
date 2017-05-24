2ed0e63700b7171f7b553f9e9f4cca1364f5b379
kvin1087 <kaustubh87@gmail.com>
2017-05-24 09:44:08 -0500

    Added socket.io connection and event functions

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
-app.get('/', function(request, response){
-  response.json({message: 'API Home Page'});
+var mongo = require('mongodb').MongoClient;
 
+app.set('port' , process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname+ 'public')));
+
+app.get('/', routes.index);
+
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' +app.get('port'));
 });
 
+io.on('connection', function(socket){
 
-app.listen(process.env.PORT || 8000, function(request, response){
-  console.log('server running');
+  console.log('A user connected');
+
+  socket.on('disconnect', function(){
+      console.log('User disconnected');
+  });
+
 });
